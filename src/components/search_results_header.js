ed80625fbf9c4abc18eb7b146a38fd8ae119e6d0
Jimeno0 <carlospj@icloud.com>
2016-12-09 17:39:36 +0100

    Artist searched binded to search header

--- src/components/search_results_header.js
+++ src/components/search_results_header.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SearchResultHeader extends Component {
   render() {
+    console.log('artist', this.props.artistSearched);
+    const { artistSearched } = this.props;
+      let header = null;
+    if (artistSearched) {
+      header = (
+        <div>
+          <span>Search for {artistSearched}</span>
+          <button>Follow artist</button>
+        </div>
+      );
+    } else {
+      header = (<span>Search Results</span>);
+    }
+
     return (
       <div className="row search-result-header">
-        <span>Search Results</span>
+        { header }
       </div>
     );
   }
 }
 
-export default SearchResultHeader;
+const mapStateToProps = (state) => ({ artistSearched: state.artist });
+
+export default connect(mapStateToProps)(SearchResultHeader);
+
+// export default SearchResultHeader;
